e3b17a954fdde48054fa232febe35dc3cc3303ac
sheenazien8 <sheenazien08@gmail.com>
2024-07-29 15:52:12 +0700

    add mysql provider

--- db/connect.go
+++ db/connect.go
@@ -3,9 +3,9 @@ package db
 import (
 	"fmt"
 	"log"
-	"strconv"
 
 	"github.com/sheenazien8/goplate/config"
+	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,27 +14,52 @@ var DB *gorm.DB
 
 func ConnectDB() {
 	var err error
-	p := config.Config("DB_PORT")
-	port, err := strconv.ParseUint(p, 10, 32)
+	var dsn string
+	var dbType = config.Config("DB_CONNECTION")
+	var db *gorm.DB
 
-	if err != nil {
-		log.Println("Idiot")
-	}
+	switch dbType {
+	case "postgres":
+		p := config.Config("DB_PORT")
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
-	DB, err = gorm.Open(
-		postgres.Open(dsn),
-		&gorm.Config{
-			DisableForeignKeyConstraintWhenMigrating: true,
-		},
-	)
-	if err != nil {
-		log.Panic("failed to connect database")
+		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			config.Config("DB_HOST"),
+			p,
+			config.Config("DB_USER"),
+			config.Config("DB_PASSWORD"),
+			config.Config("DB_NAME"),
+		)
+
+		db, err = gorm.Open(
+			postgres.Open(dsn),
+			&gorm.Config{
+				DisableForeignKeyConstraintWhenMigrating: true,
+			},
+		)
+
+	case "mysql":
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			config.Config("DB_USER"),
+			config.Config("DB_PASSWORD"),
+			config.Config("DB_HOST"),
+			config.Config("DB_PORT"),
+			config.Config("DB_NAME"),
+		)
+
+		db, err = gorm.Open(
+			mysql.Open(dsn),
+			&gorm.Config{
+				DisableForeignKeyConstraintWhenMigrating: true,
+			},
+		)
+	default:
+		log.Panic("Unsupported database type")
 	}
 
 	if err != nil {
 		log.Panic(err.Error())
 	}
+	DB = db
 
 	fmt.Println("Connection Opened to Database")
 }
